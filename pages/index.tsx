94f17e68d4da46f1cfd004999f1a8c6a35b0cb0f
Rony <ronymawad@gmail.com>
2022-07-24 13:55:33 -0400

    init project with tailwindcss jit

---
+++ pages/index.tsx
@@ -0,0 +1,25 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+
+      <Head>
+        <title>Rony Mawad's Portfolio</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main className='flex justify-center'>
+        <h1 className='text-9xl'>Hello</h1>
+      </main>
+
+      <footer>
+      </footer>
+
+    </div>
+  )
+}
+
+export default Home
