6c671bb00d5ee1fb662b8a3ba6e7c5f43e388346
Rony <ronymawad@gmail.com>
2023-06-18 16:29:43 -0400

    initial work section is made with lorem

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			'xs': '480px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+		},
+		extend: {
+			inset:{
+				'17':'68px',
+				'26':'104px'
+			}
+		},
 	},
 	plugins: [],
 }
