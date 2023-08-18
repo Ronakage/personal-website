0f4268f2dac324ae0e1d0165dfb311668194bac7
Rony <ronymawad@gmail.com>
2023-08-18 18:08:47 -0400

    finished work section for my first internship

--- src/etc/darkMode.ts
+++ src/etc/darkMode.ts
@@ -1,9 +1,9 @@
 if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark');
-    document.getElementById("btn").innerHTML="Dark Mode";
-} else {
     document.documentElement.classList.remove('dark');
     document.getElementById("btn").innerHTML="Light Mode";
+} else {
+    document.documentElement.classList.add('dark');
+    document.getElementById("btn").innerHTML="Dark Mode";
 }
 
 var darkModeButton = document.querySelector(".mode");
