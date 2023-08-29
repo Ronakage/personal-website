02833bfda9f44addb668a680cfe545b030ff0a72
Rony <ronymawad@gmail.com>
2023-08-29 12:48:50 -0400

    edit darkmode.ts

--- src/etc/darkMode.ts
+++ src/etc/darkMode.ts
@@ -1,11 +1,3 @@
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.remove('dark');
-    document.getElementById("btn").innerHTML="Light Mode";
-} else {
-    document.documentElement.classList.add('dark');
-    document.getElementById("btn").innerHTML="Dark Mode";
-}
-
 var darkModeButton = document.querySelector(".mode");
 
 function themeSwitch() {
